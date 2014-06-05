a93ccd844ffec090f925d9d1725816ed69ab9d3a
stkay <doraaki0@gmail.com>
2014-06-05 19:10:12 +0900

    ストロベリーリナックスに対応

--- rss2.rb
+++ rss2.rb
@@ -12,8 +12,9 @@ client = Twitter::REST::Client.new(
 )
 
 db = SQLite3::Database.open "ruby-rss-bot.sqlite3"
-record_akizuki = db.execute "select * from akizuki order by time desc limit 10;"  
-record_aitendo = db.execute "select * from aitendo order by time desc limit 10;"
+record_akizuki = db.execute "select * from akizuki order by time desc limit 20;"  
+record_aitendo = db.execute "select * from aitendo order by time desc limit 20;"
+record_slinux = db.execute "select * from slinux order by time desc limit 20;"
 
 def tweet(twitter_client, db, record_data, record_name, text)
 	repeated = false
@@ -33,7 +34,7 @@ end
 akizuki = FeedNormalizer::FeedNormalizer.parse open('http://shokai.herokuapp.com/feed/akizuki.rss')
 akizuki_item = akizuki.entries
 
-(2..11).each do |num|	
+(2..21).each do |num|	
 	text = akizuki_item[num].description
 	link = akizuki_item[num].url
 	ptext = Nokogiri::HTML.parse(text).xpath('//p')
@@ -48,7 +49,7 @@ end
 aitendo = FeedNormalizer::FeedNormalizer.parse open('http://www.aitendo.com/rss/rss.php')
 aitendo_item = aitendo.entries
 
-(0..9).each do |num|
+(0..19).each do |num|
 	title = aitendo_item[num].title
 	link = aitendo_item[num].url
 	aitendo_feed = title+" "+link
@@ -56,5 +57,19 @@ aitendo_item = aitendo.entries
 	tweet(client, db, record_aitendo, "aitendo", aitendo_feed)
 end
 ###aitendo end ###
+
+###strawberry-linux begin###
+uri = URI.parse 'http://pipes.yahoo.com/pipes/pipe.run?_id=43d7a8defa45bcda73071c0a157abadf&_render=rss'
+slinux = FeedNormalizer::FeedNormalizer.parse open(uri)
+slinux_item = slinux.entries
+
+(0..19).each do |num|
+	title = slinux_item[num].title
+	link = slinux_item[num].url
+	slinux_feed = title+" "+link
+
+	tweet(client, db, record_slinux, "slinux", slinux_feed)
+end
+###strawberry-linux end###
 
 p "end"
